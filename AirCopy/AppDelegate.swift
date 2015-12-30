33fccd2abb52c3ce5aae623a1bcc36cdef022634
Tamás Lustyik <elveestei@gmail.com>
2015-12-30 19:03:04 +0100

    Pimped up OutboundTransfer

--- AirCopy/AppDelegate.swift
+++ AirCopy/AppDelegate.swift
@@ -15,49 +15,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
     var statusItem: NSStatusItem!
     var currentPasteboardItems: [NSPasteboardItem] = []
-    var trampoline: ActionTrampoline<NSMenuItem>!
     var menuController: MenuController!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         AirCopyService.sharedService.start()
-
-        trampoline = ActionTrampoline<NSMenuItem> { [weak self] (item: NSMenuItem) in
-            NSLog("menu item clicked: %@", item)
-            
-            let service = item.representedObject as! NSNetService
-
-            var os: NSOutputStream? = nil
-            guard
-                service.getInputStream(nil, outputStream: &os),
-                let outputStream = os where os != nil
-            else {
-                return
-            }
-            
-            outputStream.open()
-
-            let pbItems = self!.currentPasteboardItems
-            let transfer = OutboundTransfer(outputStream: outputStream)
-            
-            itemLoop:
-            for pbItem in pbItems {
-                NSLog("types: %@", pbItem.types)
-                for type in pbItem.types {
-                    guard let itemData = pbItem.dataForType(type) else {
-                        continue
-                    }
-                    do {
-                        try transfer.transferItemWithType(type, data: itemData)
-                    }
-                    catch {
-                        break itemLoop
-                    }
-                }
-            }
-
-            outputStream.close()
-
-        }
 
         let statusBar = NSStatusBar.systemStatusBar()
         statusItem = statusBar.statusItemWithLength(NSVariableStatusItemLength)
--- AirCopy/OutboundTransfer.swift
+++ AirCopy/OutboundTransfer.swift
@@ -13,6 +13,18 @@ enum IOError: ErrorType {
 }
 
 extension NSOutputStream {
+    func writeUInt8(value: UInt8) throws -> Int {
+        let ptr = UnsafeMutablePointer<UInt8>.alloc(1)
+        defer { ptr.dealloc(1) }
+        ptr.initialize(value)
+        
+        let count = write(ptr, maxLength: 1)
+        if count < 1 {
+            throw IOError.Unknown
+        }
+        return 1
+    }
+    
     func writeUInt64(value: UInt64) throws -> Int {
         let ptr = UnsafeMutablePointer<UInt64>.alloc(1)
         defer { ptr.dealloc(1) }
@@ -45,18 +57,59 @@ extension NSOutputStream {
 }
 
 
+protocol OutboundTransferDelegate: class {
+    func outboundTransferDidEnd(transfer: OutboundTransfer)
+}
+
 class OutboundTransfer {
+    private let _netService: NSNetService
+    var netService: NSNetService { return _netService }
     
-    private let _outputStream: NSOutputStream
+    private let _payload: [[(String, NSData)]]
+    weak var delegate: OutboundTransferDelegate? = nil
     
-    init(outputStream: NSOutputStream) {
-        _outputStream = outputStream
+    init(netService: NSNetService, payload: [[(String, NSData)]]) {
+        _netService = netService
+        _payload = payload
     }
     
-    func transferItemWithType(type: String, data: NSData) throws {
-        try _outputStream.writeUTF8String(type)
-        try _outputStream.writeData(data)
+    func start() {
+        defer { self.delegate?.outboundTransferDidEnd(self) }
+        
+        var os: NSOutputStream? = nil
+        guard
+            _netService.getInputStream(nil, outputStream: &os),
+            let outputStream = os where os != nil
+        else {
+            return
+        }
+        
+        outputStream.open()
+
+        do {
+            for pbItemReps in _payload {
+                try outputStream.writeUInt8(UInt8(pbItemReps.count))
+                for (repType, repData) in pbItemReps {
+                    try outputStream.writeUTF8String(repType)
+                    try outputStream.writeData(repData)
+                }
+            }
+        }
+        catch {
+        }
+
+        outputStream.close()
     }
     
+}
+
+func == (lhs: OutboundTransfer, rhs: OutboundTransfer) -> Bool {
+    return lhs.netService == rhs.netService
+}
+
+extension OutboundTransfer: Hashable {
+    var hashValue: Int {
+        return _netService.hashValue
+    }
 }
 
